b56606fc257b83befd3adeea3889620b6e103b99
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2015-07-25 09:14:05 -0700

    Initial Commit

---
+++ swift/UseObjcLibWithCategory/UseObjcLibWithCategory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UseObjcLibWithCategory
+//
+//  Created by Xiao Xiao on 7/25/15.
+//  Copyright (c) 2015 Xiao Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
